0995a6474fb61f6611e9eabb7fa5e4df98a92fe7
JJH <jukka.j.hirvonen@gmail.com>
2023-12-19 11:28:06 +0200

    Table for new node

--- db/init/1_init_tables.sql
+++ db/init/1_init_tables.sql
@@ -6,3 +6,12 @@ CREATE TABLE measurements (
 	humidity       numeric,
 	pressure       numeric
 );
+
+CREATE TABLE measurements2 (
+    id     bigint not null GENERATED ALWAYS AS IDENTITY,
+    device_id      varchar,
+    timestamp       date,
+    temperature       numeric,
+    humidity       numeric,
+    waterleak       numeric
+);
