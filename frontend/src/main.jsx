b4fd007b3ca4e693b9898186df6f4a604653fe27
JJH <jukka.j.hirvonen@gmail.com>
2024-01-08 13:58:15 +0200

    Added example router and included axios + react-router-dom into package

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/", // The root path
+    element: <App />, // Main layout component for the root path
+    //errorElement: <NoPage />, // Component to render in case of an error
+    children: [
+      /*{
+        path: "asd",
+        element: <asd />
+      }, {
+        path: "asd2",
+        element: <asd2 />
+      } */
+    ]
+  }
+])
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} /> {/* Provide the router to the application */}
+  </React.StrictMode>
+);
